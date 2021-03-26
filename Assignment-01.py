05337151b2634d4d49d2d42d0d4c3aca8de032a2
Ashutosh Pandey <79349012+Ashu62@users.noreply.github.com>
2021-03-26 15:26:50 +0530

    Add files via upload

---
+++ Assignment-01.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 1.	Write a Python program to print "Hello Python"?
+
+# In[1]:
+
+
+print("Hello Python")
+
+
+# ### 2.	Write a Python program to do arithmetical operations addition and division.
+
+# In[2]:
+
+
+x = int(input())
+y = int(input())
+Add = x+y
+Div = x/y
+print(Add)
+print(Div)
+
+
+# ### 3.	Write a Python program to find the area of a triangle.
+
+# In[2]:
+
+
+b = int(input("Enter base of triangle (b) : "))
+h = int(input("Enter the height of triangle (h) : "))
+A = (b*h)/2
+print("Area of triangle is ", A)
+
+
+# ### 4.	Write a Python program to swap two variables?
+
+# In[ ]:
+
+
+m = int(input())
+n = int(input())
+z = m
+m = n
+n = z
+print("new value of m", m)
+print("new value of n",n)
+
+
+# ### 5.	Write a Python program to generate a random number?
+
+# In[1]:
+
+
+u = int(input())
+v = int(input())
+num = (10*u+v)
+print(num)
+
+
+# In[ ]:
+
+
+
+
